b488a510776b55d21844d86141263c4c4bc3d7f8
Peter Pan <peter@never.lan>
2024-10-29 22:44:37 -0400

    rollback changes to find JSON error

--- dpak_gen.sh
+++ dpak_gen.sh
@@ -13,13 +13,14 @@ rsync -avh ./sources/datapaks/terralith_mc1-21-1/data/biom_tag_villagers ./data/
 rsync -avh ./sources/datapaks/terralith_mc1-21-1/data/terralith ./data/
 rsync -avh ./sources/datapaks/terralith_mc1-21-1/data/c ./data/
 #>> Add fortified villages:
+mkdir -p ./sources/datapaks/terralith_mc1-21-1/data/minecraft/tags/worldgen/structure
 rsync -avh ./sources/datapaks/terralith_mc1-21-1/data/minecraft/tags/worldgen/structure/ ./data/minecraft/worldgen/structure/
 # https://modrinth.com/datapack/hopo-better-underwater-ruins
-rsync -avh ./sources/datapaks/hopouwruins_mc1-21-1/data/ ./data/
+#rsync -avh ./sources/datapaks/hopouwruins_mc1-21-1/data/ ./data/
 # https://modrinth.com/datapack/fancier-mansions
-rsync -avh ./sources/datapaks/fancyman_mc1-21-1/data/ ./data/
+#rsync -avh ./sources/datapaks/fancyman_mc1-21-1/data/ ./data/
 # https://modrinth.com/datapack/incendium ; terralith for the Nether
-rsync -avh ./sources/datapaks/incendium_mc1-21-1/data/ ./data/
+#rsync -avh ./sources/datapaks/incendium_mc1-21-1/data/ ./data/
 # https://modrinth.com/datapack/nullscape ; terralith for the End
 rsync -avh ./sources/datapaks/nullscape_mc1-21-1/data/ ./data/
 # https://modrinth.com/datapack/true-ending
@@ -32,6 +33,7 @@ rsync -avh ./sources/datapaks/tectonic_mc1-21-0/data/ ./data/
 #>> only the dark forest (-_-)
 rsync -avh ./sources/datapaks/wwoo_mc1-21-0/data/wythers ./data/
 rsync -avh ./sources/datapaks/wwoo_mc1-21-0/1-21-overlay/data/wythers/ ./data/wythers/
+mkdir -p ./sources/datapaks/terralith_mc1-21-1/data/minecraft/worldgen/biome
 rsync -avh ./sources/datapaks/wwoo_mc1-21-0/1-21-overlay/data/minecraft/worldgen/biome/dark_forest.json ./data/minecraft/worldgen/biome/
 #>> and tepui's /^\
 rsync -avh ./sources/datapaks/towertepui_mc1-21-0/data/ ./data/
