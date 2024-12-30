16a2792f3de1b15342a8e859e0dbd7dddb87b71e
Peter Pan <peter@never.lan>
2024-12-30 16:33:20 -0500

    add xaeros icons for freshanims

--- rpak_gen.sh
+++ rpak_gen.sh
@@ -31,5 +31,7 @@ rsync -avh ./sources/resourcepaks/tooltrims_v2-2-2a/assets/ ./assets/
 rsync -avh ./sources/resourcepaks/roundista_128xR29/assets/ ./assets/
 # https://modrinth.com/resourcepack/fresh-animations
 rsync -avh ./sources/resourcepaks/freshanims_v1-9-2/assets/ ./assets/
+# https://modrinth.com/resourcepack/icon-xaeros-x-freshanimations
+rsync -avh ./sources/resourcepaks/faxaeros_v2-4/assets/ ./assets/
 ### always keep custom changes last..
 rsync -avh ./sources/resourcepaks/custom_overlay_mc1-21-1/assets/ ./assets/
