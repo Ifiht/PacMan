330baf5235e715f0d8f4f1d4a2e0bd08abe0425d
Peter Pan <peter@never.lan>
2024-12-30 15:51:06 -0500

    add fresh animations

--- rpak_gen.sh
+++ rpak_gen.sh
@@ -29,5 +29,7 @@ rsync -avh ./sources/resourcepaks/vanillatweaks_mc1-21-1/assets/ ./assets/
 rsync -avh ./sources/resourcepaks/tooltrims_v2-2-2a/assets/ ./assets/
 # https://modrinth.com/resourcepack/roundista
 rsync -avh ./sources/resourcepaks/roundista_128xR29/assets/ ./assets/
+# https://modrinth.com/resourcepack/fresh-animations
+rsync -avh ./sources/resourcepaks/freshanims_v1-9-2/assets/ ./assets/
 ### always keep custom changes last..
 rsync -avh ./sources/resourcepaks/custom_overlay_mc1-21-1/assets/ ./assets/
